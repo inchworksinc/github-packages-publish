3d1e9c4d4dd8f1945bfecec71b2e70edfb12bfb3
Roman Pena <roman.pena@inchworks.net>
2023-02-27 15:51:41 -0500

    Initial commit

---
+++ src/main/java/net/inchworks/samples/publishgithubpackagedemo/PublishGithubPackageDemoApplication.java
@@ -0,0 +1,13 @@
+package net.inchworks.samples.publishgithubpackagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishGithubPackageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishGithubPackageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/inchworks/samples/publishgithubpackagedemo/PublishGithubPackageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.inchworks.samples.publishgithubpackagedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishGithubPackageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
